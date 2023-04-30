52b10ac8edb51829e7845aacd3560261d502f771
Егор <Egorog163@yandex.ru>
2023-04-30 14:46:37 +0300

    fix:app.prop + whitespaces

--- service/src/main/java/ru.practicum.explore_with_me/request/repository/RequestRepository.java
+++ service/src/main/java/ru.practicum.explore_with_me/request/repository/RequestRepository.java
@@ -10,17 +10,16 @@ import ru.practicum.explore_with_me.user.model.User;
 import java.util.List;
 
 public interface RequestRepository extends JpaRepository<Request, Long> {
-   @Query(value = "select r from Request r where r.requester = :requester")
-    List<Request> findRequestByRequester( @Param("requester") User requester); //@Param("requester")
+    @Query(value = "select r from Request r where r.requester = :requester")
+    List<Request> findRequestByRequester(@Param("requester") User requester); //@Param("requester")
 
     @Query(value = "select r from Request r where r.event = :event")
-    List<Request> findRequestsByEventAndRequester( @Param("event") Event event ); //@Param("ru.practicum.explore_with_me/event")
+    List<Request> findRequestsByEventAndRequester(@Param("event") Event event); //@Param("ru.practicum.explore_with_me/event")
 
-     @Query(value = "select r from Request r where r.id in :requestIds")
-   List<Request> findRequestsByRequestIds( @Param("requestIds") List<Long> requestIds); //@Param("requestIds")
+    @Query(value = "select r from Request r where r.id in :requestIds")
+    List<Request> findRequestsByRequestIds(@Param("requestIds") List<Long> requestIds); //@Param("requestIds")
 
     @Query(value = "select count(*) from requests where requester_id = :requester_id and event_id = :event_id", nativeQuery = true)
     Integer getCountRequestsForEventAndUser(@Param("requester_id") Long requesterId, @Param("event_id") Long eventId);
-
 
 }
