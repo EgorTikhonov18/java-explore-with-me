9ee4c95a0e932c41c25aef23ee020442b838779b
Егор <Egorog163@yandex.ru>
2023-05-14 13:54:09 +0300

    refactor project

--- service/src/main/java/ru.practicum.explore_with_me/constant/EventStats.java
+++
@@ -1,6 +0,0 @@
-package ru.practicum.explore_with_me.constant;
-
-public enum EventStats {
-    EVENTS,
-    EVENT
-}
--- service/src/main/java/ru.practicum.explore_with_me/constant/RequestStatus.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.explore_with_me.constant;
-
-public enum RequestStatus {
-    PENDING,
-    CONFIRMED,
-    REJECTED,
-    CANCELED
-}
